7e8efb44794bb08d197df09576049ba099ce562b
lughjw <lughjw@gmail.com>
2020-04-06 17:24:40 -0500

    Done.

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        { props.sushi.map(sushi => 
+            <Sushi 
+              key={sushi.id} 
+              handleEatSushi={props.handleEatSushi} 
+              sushi={sushi}
+            />
+          )
         }
-        <MoreButton />
+        <MoreButton handleGetMoreSushi={props.handleGetMoreSushi}/>
       </div>
     </Fragment>
   )
